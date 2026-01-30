72d0ad6915ba72964284e47cf294f2833e4eb607
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2026-01-30 13:03:54 +0530

    feat:DetailedCountries Header Routing

--- src/app/pages/detailed-page/detailed-page.ts
+++ src/app/pages/detailed-page/detailed-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Country } from '../../models/country';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountryService } from '../../services/country-service';
 
 @Component({
   selector: 'app-detailed-page',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './detailed-page.html',
   styleUrl: './detailed-page.css',
 })
-export class DetailedPage {
+export class DetailedPage implements OnInit {
+  country?: Country;
+  private route =inject(ActivatedRoute);
+  private router=inject(Router);
+  private countryService = inject(CountryService);
+  isLoading = signal(true);
+  ngOnInit() {  // Lifecycle hook that is called after data-bound properties of a directive are initialized
+    const code = this.route.snapshot.paramMap.get('countryCode');  // Get the country code from the route parameters
+    if(!code) return;
+    this.countryService.getCountryByAlpha2Code(code).subscribe((data) => {  // data refers to the country found
+      if(data)
+      {
+        this.country = data;
+        console.log(this.country);
+
+      }
+      this.isLoading.set(false);
+      
+    });
+  }
+  goBack() {
+    this.router.navigate(['/']);
+  }
+
+
+  
 
 }
