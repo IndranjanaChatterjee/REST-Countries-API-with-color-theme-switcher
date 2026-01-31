039b723bd83825fd29a67660cec917f478c0a48e
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2026-01-31 12:05:54 +0530

    feat:Search_Count

--- src/app/pages/main-section/main-section.ts
+++ src/app/pages/main-section/main-section.ts
@@ -1,27 +1,41 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 import { Country } from '../../models/country';
 import { CountryService } from '../../services/country-service';
 import { RouterLink } from '@angular/router';
-import { Header } from '../header/header';
+
 
 @Component({
   selector: 'app-main-section',
-  imports: [RouterLink,Header],
+  imports: [RouterLink],
   templateUrl: './main-section.html',
   styleUrl: './main-section.css',
 })
 export class MainSection implements OnInit {
-  countries: Country[] = [];
+  countries = signal<Country[]>([]);
   isLoading = signal(true);
+  searchTerm = signal('');
 
   
   private countryService = inject(CountryService);;
+  filteredCountries = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+
+    if (!term) return this.countries();
+
+    return this.countries().filter(country =>
+      country.name.toLowerCase().includes(term)
+    );
+  });
 
   ngOnInit() {
     this.countryService.getCountries().subscribe((data) => {
-      this.countries = data;
+      this.countries.set(data);
       this.isLoading.set(false);
       console.log(this.countries);
     });
+  }
+
+  onSearch(value: string) {
+    this.searchTerm.set(value);
   }
 }
